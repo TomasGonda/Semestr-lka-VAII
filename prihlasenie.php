9a8166c2e426aa539db030033ec025519a17a11f
TomasGonda <tomasgonda2@gmail.com>
2024-12-04 14:15:50 +0100

    Php na login

--- prihlasenie.php
+++ prihlasenie.php
@@ -1,1 +1,28 @@
+<?php
 
+require "db_pripoj.php";
+
+if ($_SERVER["REQUEST"] == "POST") {
+    $email = $_POST['email'];
+    $heslo = $_POST['heslo'];
+
+    $sql = "SELECT * FROM uzivatelia WHERE email = 'email'";
+    $vysledok = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($vysledok) > 0) {
+        $uzivatel = mysqli_fetch_array($vysledok);
+
+        if (password_verify($heslo, $uzivatel['heslo_hasch'])) {
+            echo "Prihlasenie bolo uspesne"
+        } else {
+            echo "Nespravne heslo";
+        }
+    }
+    else {
+        echo "Tento email neexistuje";
+    } 
+} else {
+    echo "Neplatná akcia";
+}
+
+sqli_close($conn);
